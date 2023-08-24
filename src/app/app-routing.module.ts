c05c50a0514470948919947cd4f7119472104184
joaquimchianca <joaquimchianca@gmail.com>
2023-08-24 18:23:00 -0300

    feat: rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
 
-const routes: Routes = [];
+// representa as rotas da aplicação
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listarPensamentos',
+    pathMatch: 'full' // pega toda url http://localhost:4200
+  },
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentoComponent
+  },
+  {
+    path: 'listarPensamentos',
+    component: ListarPensamentosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
